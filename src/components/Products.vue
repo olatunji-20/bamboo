3b461eabed19bbdbc0bb3bbd5c9677068578ce9c
falana sheriff <falanasheriffdeen@gmail.com>
2023-06-13 12:04:31 +0100

    work on products session

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -7,6 +7,9 @@
             <li>SOFA</li>
             <li>BENCH</li>
         </ul>
+        <div class="small">
+            <div class="subsmall"></div>
+        </div>
     </section>
 </template>
 
@@ -14,7 +17,7 @@
 section {
     border: 5px solid blue;
     width: 100%;
-    height: 400px;
+    height: 500px;
     text-align:center;
 }
 h1 {
@@ -25,5 +28,18 @@ li {
     display: inline-block;
     margin: 0px 25px;
     font-weight: bolder;
+}
+.small {
+    border: 2px solid red;
+    height: 250px;
+    width: 80%;
+    margin: 50px auto 0px;
+    padding: 5px;
+    overflow: auto;
+}
+.subsmall {
+    border: 1px solid green;
+    height: 230px;
+    width: 1500px;
 }
 </style>
